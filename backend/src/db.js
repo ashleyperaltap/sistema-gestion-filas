987a9d8833b3aef6d1932399feb95c72aa6cfc2c
ashleyperaltap <ashlperalta0@gmail.com>
2026-08-10 23:45:17 -0400

    Modify PostgreSQL connection string in db.js
    Updated the connection string for the PostgreSQL database.

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
+  postgresql://gestion_filas_db_user:L2WcdGfS4vwy6fpePBcx6wCkJsQHgKTx@dpg-d9t5l7afngtc73cpsi30-a/gestion_filas_db
+    
   connectionString: process.env.DATABASE_URL || "postgresql://gestion_filas_db_user:L2WcdGfS4vwy6fpePBcx6wCkJsQHgKTx@dpg-d9t517afngtc73cpsi30-a.ohio-postgres.render.com/gestion_filas_db",
   ssl: {
     rejectUnauthorized: false
