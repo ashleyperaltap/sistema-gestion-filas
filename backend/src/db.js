c06c5eea682a45f73e73fe5ef0b80914e6c5ff55
ashleyperaltap <ashlperalta0@gmail.com>
2026-08-10 23:48:07 -0400

    Update database connection string for db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "dpg-d9t517afngtc73cpsi30-a",
+  connectionString: process.env.DATABASE_URL || "postgresql://gestion_filas_db_user:L2WcdGfS4vwy6fpePBcx6wCkJsQHgKTx@dpg-d9t5l7afngtc73cpsi30-a/gestion_filas_db",
   ssl: {
     rejectUnauthorized: false
   }
