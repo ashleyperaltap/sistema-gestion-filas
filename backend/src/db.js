7c34bd6f5f1e35c4919cd99abb187070da15edf5
ashleyperaltap <ashlperalta0@gmail.com>
2026-08-10 23:14:29 -0400

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,10 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
-// Pool de conexiones a PostgreSQL. Reutiliza las variables del archivo .env
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL || "postgresql://gestion_filas_db_user:L2WcdGfS4vwy6fpePBcx6wCkJsQHgKTx@dpg-d9t517afngtc73cpsi30-a.ohio-postgres.render.com/gestion_filas_db",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
