a990b17f65c8ff210d4c32169f108efb6268981c
Ashleyperaltap <ashlperalta0@gmail.com>
2026-08-24 08:40:40 -0400

    Rediseno visual del panel: tarjetas de colores y nueva tipografia

---
+++ backend/fix_acentos.sql
@@ -0,0 +1,3 @@
+UPDATE servicios SET nombre = U&'Caja de atenci\00F3n', descripcion = 'Pagos y cobros generales' WHERE id = 1;
+UPDATE servicios SET nombre = U&'Informaci\00F3n', descripcion = U&'Consultas generales y orientaci\00F3n' WHERE id = 2;
+UPDATE servicios SET nombre = 'Servicio al cliente', descripcion = 'Reclamos, quejas y solicitudes' WHERE id = 3;
